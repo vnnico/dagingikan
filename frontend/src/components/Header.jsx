5f1f8c3a04597de90748edd0ea9c80936c952b5f
vnnico <nicson7585@gmail.com>
2024-09-13 14:45:47 +0700

    feat: Cart's Count Notification

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import Cart from "../components/Cart";
 import { useState } from "react";
 
 const Header = () => {
-  const { isLoggedIn, toggleCart } = useAppContext();
+  const { isLoggedIn, toggleCart, carts } = useAppContext();
 
   return (
     <div className="bg-gray-950 py-6 md:text-3xl max-md:px-3 text-xl ">
@@ -25,10 +25,17 @@ const Header = () => {
                 <BsCardText></BsCardText>
               </Link>
               <button
-                className="flex items-center  text-white px-3 font-bold hover:text-yellow-300 "
+                className="flex items-center  text-white px-3 font-bold hover:text-yellow-300 relative "
                 onClick={toggleCart}
               >
                 <BsCart4></BsCart4>
+                {carts && carts.length > 0 && (
+                  <div className=" absolute top-0 right-1 text-xs flex">
+                    <p className="bg-red-500 min-w-[16px] min-h-[13px] rounded ">
+                      {carts.length}
+                    </p>
+                  </div>
+                )}
               </button>
               <Link
                 to="/profile"
